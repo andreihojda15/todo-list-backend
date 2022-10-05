0e3a5fe714023471b9874c8c5a3ffdcd2e27d5f0
DESKTOP-5A0R7DV\andre <andrei.hojda00@gmail.com>
2022-10-06 00:43:49 +0300

    [BE] boilerplate: installed mongoose

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose')
 
 const app = express();
 
 const port = 3000
+
+main = async () => {
+  await mongoose.connect('mongodb://localhost:27017')
+}
+
+main().then(() => console.log('connected to mongodb')).catch(err => console.log(err))
 
 app.get('/', (req, res) => {
   return res.send('hello from root');
